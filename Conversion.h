0fa95756c4e2e773bd0229c12d6294519950600b
shohnq <shohn.q@gmail.com>
2015-02-08 14:42:16 +0800

    Update Conversion.h

--- Conversion.h
+++ Conversion.h
@@ -2,3 +2,8 @@
 #Convert hex string (char []) to int
 const char *hexstring = "abcdef0";
 int number = (int)strtol(hexstring, NULL, 16);
+
+# int to hex string
+std::stringstream stream;
+stream << std::hex << your_int;
+std::string result( stream.str() );
